c2090b530b776a417460d836df65acf3f7621408
Mitch <mitch@grabbrapp.io>
2023-12-15 16:23:27 -0600

    Adding bookmarks

--- dan_server/src/datatype_mgmt/mod.rs
+++ dan_server/src/datatype_mgmt/mod.rs
@@ -14,7 +14,8 @@ impl datatype_mgmt {
     // Right now what we want to do is be able to manipulate data types for nodes
     #[oai(path="/datatype/get" ,method="get")]
     async fn datatype_get(&self) -> datatype_get {
-
+        // TODO: Have this iterate over all of the files in the datatypes directory
+        // instead of just the one JSON file
         let fileloc = "./datatypes/datatypes.json";
         let readstr = std::fs::read_to_string(fileloc.to_string()).unwrap();
         let tf : typelist = serde_json::from_str(&readstr).unwrap(); 
@@ -26,10 +27,10 @@ impl datatype_mgmt {
     }
 
     // TODO: Create a POST for data types
-    // Let's just add a new file and abstract the datatype read away from one file and into
-    // multiple files
+    
     #[oai(path="/datatype/put", method="post")]
     async fn datatype_post(&self) -> datatype_put {
+        // TODO: Figure out a schema for creating new types in existing files
         return datatype_put::Ok(poem_openapi::payload::Json(datatype_put_ok {
             message: "yeet".to_string()
         }))
